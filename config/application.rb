5ddf4d581dbb396b7a11fb56e139790367a52829
mdas.rafael <mdas.rafael@gmail.com>
2019-09-08 14:15:15 +0200

    Protect sensitive information in env variables

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,12 @@ module UserRegistration
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Load environment variables
+    config.before_configuration do
+		  env_file = File.join(Rails.root, 'config', ENV['RAILS_ENV']+'.yml')
+		  YAML.load(File.open(env_file)).each do |key, value|
+		    ENV[key.to_s] = value
+		  end if File.exists?(env_file)
+		end
   end
 end
