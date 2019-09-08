7bd8b2d2afbb8e56d99620d38d6bea0547baf430
mdas.rafael <mdas.rafael@gmail.com>
2019-09-08 05:44:38 +0200

    Fix routes file to the last state

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	root 'sessions#new'
+
+  resources :users, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :passwords, only: [:new, :forgot, :reset]
+
+  get 'signup', to: 'users#new'
+	get 'profile', to: 'users#edit'
+	put 'update_profile', to: 'users#update'
+	put 'update_password', to: 'users#update_password'
+  get 'login', to: 'sessions#new'
+	post 'login_attempt', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+  get 'forgot_password', to: 'passwords#forgot'
+	get 'confirm_token', to: 'passwords#confirm_token'
+	post 'create_token', to: 'passwords#create_token'
+	get 'reset_password', to: 'passwords#reset'
 end
